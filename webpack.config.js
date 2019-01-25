43ae10cb331be60108d3156c37b2023dcd4878cf
Vlad Jerca <vlad.jerca@gmail.com>
2019-01-25 02:03:16 +0200

    chore: configure project infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
+
+const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
+
+const buildFolder = 'build';
+
+const env = process.env['NODE_ENV'];
+const isDevelopment = process.env['NODE_ENV'] === 'development';
+
+module.exports = {
+  mode: env,
+  watch: isDevelopment,
+  entry: {
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
+    content: path.join(__dirname, 'src', 'js', 'content.js'),
+    background: path.join(__dirname, 'src', 'js', 'background.js'),
+  },
+  output: {
+    path: path.join(__dirname, buildFolder),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
+        loader: 'file-loader?name=[name].[ext]',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new ChromeExtensionReloader({
+      port: 9090,
+      reloadPage: true,
+      entries: {
+        contentScript: 'content',
+        background: 'background',
+      }
+    }),
+    new CleanWebpackPlugin(buildFolder),
+    new CopyWebpackPlugin([{
+      from: './src/**/*.png',
+      flatten: true,
+    }, {
+      from: 'src/manifest.json',
+      transform: (content) => {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(
+          JSON.stringify({
+            description: process.env.npm_package_description,
+            version: process.env.npm_package_version,
+            ...JSON.parse(content.toString())
+          })
+        )
+      }
+    }]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new WriteFilePlugin()
+  ]
+};
