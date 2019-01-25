60c87317121b93f728e8ba774fbad37f624e45fd
Vlad Jerca <vlad.jerca@gmail.com>
2019-01-25 15:44:58 +0200

    chore: add packaging mechanism and bump version @ 0.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
+const CrxWebpackPlugin = require('./.webpack/plugins/crx-pack');
 
 const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
 
@@ -11,6 +12,52 @@ const buildFolder = 'build';
 
 const env = process.env['NODE_ENV'];
 const isDevelopment = process.env['NODE_ENV'] === 'development';
+
+const plugins = {
+  common: [
+    new CleanWebpackPlugin(buildFolder),
+    new CopyWebpackPlugin([{
+      from: './src/**/*.png',
+      flatten: true,
+    }, {
+      from: 'src/manifest.json',
+      transform: (content) => {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(
+          JSON.stringify({
+            description: process.env.npm_package_description,
+            version: process.env.npm_package_version,
+            ...JSON.parse(content.toString())
+          })
+        )
+      }
+    }]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new WriteFilePlugin(),
+  ],
+  development: [
+    new ChromeExtensionReloader({
+      port: 9090,
+      reloadPage: true,
+      entries: {
+        contentScript: 'content',
+        background: 'background',
+      }
+    }),
+  ],
+  production: [
+    new CrxWebpackPlugin({
+      keyFile: 'build.pem',
+      contentPath: buildFolder,
+      outputPath: 'dist',
+      name: `${process.env.npm_package_name}-${process.env.npm_package_version}`
+    })
+  ]
+};
 
 module.exports = {
   mode: env,
@@ -42,37 +89,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new ChromeExtensionReloader({
-      port: 9090,
-      reloadPage: true,
-      entries: {
-        contentScript: 'content',
-        background: 'background',
-      }
-    }),
-    new CleanWebpackPlugin(buildFolder),
-    new CopyWebpackPlugin([{
-      from: './src/**/*.png',
-      flatten: true,
-    }, {
-      from: 'src/manifest.json',
-      transform: (content) => {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(
-          JSON.stringify({
-            description: process.env.npm_package_description,
-            version: process.env.npm_package_version,
-            ...JSON.parse(content.toString())
-          })
-        )
-      }
-    }]),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'popup.html'),
-      filename: 'popup.html',
-      chunks: ['popup']
-    }),
-    new WriteFilePlugin()
-  ]
+  plugins: [...plugins.common, ...plugins[env]]
 };
